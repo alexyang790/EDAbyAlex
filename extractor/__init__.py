ffa1dd8ef200917547235064bc1221697d656f27
Alex Yang <alexyoung0204@gmail.com>
2025-02-04 15:08:57 -0500

    adding files

---
+++ extractor/__init__.py
@@ -0,0 +1,14 @@
+"""
+Extractor Package
+=================
+
+This package provides tools for extracting data from various sources.
+
+Modules:
+    - module1: Description of module1
+    - module2: Description of module2
+"""
+
+# Define package-level variables or functions if needed
+__version__ = '0.1.0'
+__author__ = 'Alex Yang'
