fc775eea59a04ecf3af771d2385a420c5ab7e5e9
Alex Yang <alexyoung0204@gmail.com>
2025-02-06 19:36:50 -0500

    Adding export feature to the package

--- extractor/base.py
+++ extractor/base.py
@@ -1,11 +1,13 @@
 import pandas as pd 
 import numpy as np
+import os 
 import glob 
 from .eda_feature import eda_feature
 from .loader_feature import loader_feature
 from .unique_val_feature import unique_val_feature
+from .export_feature import export_feature
 
-class extractor(eda_feature, loader_feature, unique_val_feature):
+class extractor(eda_feature, loader_feature, unique_val_feature, export_feature):
     def __init__(self, path, file_type = None, sheet_name = None, file_query = None):
         
         self.path = path
